91eb9343cb0b42b7f2b0de5183694f1968628aee
joferava <jfelipe221095@gmail.com>
2021-02-27 16:05:43 -0600

    Initial Commit

---
+++ SwiftState/SwiftStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStateApp.swift
+//  SwiftState
+//
+//  Created by Felipe Ramirez Vargas on 27/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
